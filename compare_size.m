20fe75f1df11d5aca8c790d020e3f95f29ddbfa6
HaraldNordgren <haraldnordgren@gmail.com>
2015-05-17 11:25:42 +0200

    better subplots and data tables

--- compare_size.m
+++
@@ -1,4 +0,0 @@
-function ratio = compare_size(original_size, file)
-
-d = dir(file);
-ratio = d.bytes / original_size;
--- normalize_matrix.m
+++
@@ -1,4 +0,0 @@
-function M_norm = normalize_matrix(M)
-
-max_val = max(M(:));
-M_norm = M / max_val;
